73b09b0571d4066e7031ee3960eebb4b79d7eeda
Loxsete <gduck3029@gmail.com>
2025-04-08 21:41:31 +0300

    Update calc.asm
    The code was rewritten under 0x7c00

--- calc.asm
+++ calc.asm
@@ -1,120 +1,27 @@
-section .text
-    org 0x100
+org 0x7c00
 
-_start:
-    mov ax, cs
+start:
+    mov ax, 0
     mov ds, ax
     mov es, ax
-
-    mov ah, 09h
-    mov dx, msg1
-    int 21h
-
-    mov ah, 0Ah
-    mov dx, num1
-    int 21h
-
-    mov ah, 09h
-    mov dx, msg2
-    int 21h
-
-    mov ah, 01h
-    int 21h
-    mov [op], al
-
-    mov ah, 09h
-    mov dx, newline
-    int 21h
-
-    mov ah, 09h
-    mov dx, msg3
-    int 21h
-
-    mov ah, 0Ah
-    mov dx, num2
-    int 21h
-
-    mov si, num1 + 2
-    call atoi
-    mov bx, ax
-
-    mov si, num2 + 2
-    call atoi
-    mov cx, ax
-
-    mov al, [op]
-    cmp al, '+'
-    je do_add
-    cmp al, '-'
-    je do_sub
-    cmp al, '*'
-    je do_mul
-    cmp al, '/'
-    je do_div
-
-    mov ah, 09h
-    mov dx, msg_err
-    int 21h
-    jmp exit
-
-do_add:
-    add bx, cx
-    jmp calculate_done
-
-do_sub:
-    sub bx, cx
-    jmp calculate_done
-
-do_mul:
-    mov ax, bx
-    mul cx
-    mov bx, ax
-    jmp calculate_done
-
-do_div:
-    mov ax, bx
-    xor dx, dx
-    div cx
-    mov bx, ax
-    jmp calculate_done
-
-calculate_done:
-    mov ah, 09h
-    mov dx, msg4
-    int 21h
-
-    mov ax, bx
+    mov ax, 5
+    mov bx, 3
+    add ax, bx
     mov di, result
     call itoa
+    mov si, result
+    call print_string
+    jmp $
 
-    mov ah, 09h
-    mov dx, result
-    int 21h
-
-    mov ah, 09h
-    mov dx, newline
-    int 21h
-
-exit:
-    mov ah, 4Ch
-    mov al, 0
-    int 21h
-
-atoi:
-    xor ax, ax
-    xor bx, bx
-atoi_loop:
-    mov bl, [si]
-    cmp bl, 13
-    je atoi_done
-    cmp bl, 0
-    je atoi_done
-    sub bl, '0'
-    imul ax, 10
-    add ax, bx
-    inc si
-    jmp atoi_loop
-atoi_done:
+print_string:
+    mov ah, 0x0e
+print_loop:
+    lodsb
+    cmp al, 0
+    je print_done
+    int 0x10
+    jmp print_loop
+print_done:
     ret
 
 itoa:
@@ -128,25 +35,16 @@ itoa_loop1:
     inc cx
     test ax, ax
     jnz itoa_loop1
-
 itoa_loop2:
     pop ax
     mov [di], al
     inc di
     loop itoa_loop2
-    mov byte [di], '$'
+    mov byte [di], 0
     ret
 
 section .data
-    msg1 db "Enter the first number: $"
-    msg2 db "Enter the operation (+, -, *, /): $"
-    msg3 db "Enter the second number: $"
-    msg4 db "Result: $"
-    msg_err db "Error: unknown operation$"
-    newline db 13, 10, '$'
+result resb 10
 
-section .bss
-    num1 resb 10
-    num2 resb 10
-    op   resb 1
-    result resb 10
+times 510-($-$$) db 0
+dw 0xAA55
